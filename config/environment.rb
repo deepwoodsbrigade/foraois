78026ccd59b34a0213567a01af13278ee9c84e35
Michael Christenson II <m3talsmith@gmail.com>
2009-09-21 13:48:55 -0400

    added haml, sass, and compass

--- config/environment.rb
+++ config/environment.rb
@@ -7,6 +7,8 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
+  config.gem 'chriseppstein-compass', :lib => 'compass', :source => 'http://gems.github.com/'
+  config.gem 'haml', :lib => 'haml', :version => '>=2.2.0'
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
