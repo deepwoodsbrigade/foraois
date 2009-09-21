f478df00d2a6983956869d32879bc382e24dec5c
Michael Christenson II <m3talsmith@gmail.com>
2009-09-21 13:55:31 -0400

    added required gems for haml,sass,compass,and cucumber

--- config/environment.rb
+++ config/environment.rb
@@ -7,8 +7,37 @@ RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'chriseppstein-compass', :lib => 'compass', :source => 'http://gems.github.com/'
-  config.gem 'haml', :lib => 'haml', :version => '>=2.2.0'
+  github = "http://gems.github.com" # to use for sources
+  
+  config.gem "rspec",
+    :lib      => false,
+    :version  => ">= 1.2.0"
+  config.gem "rspec-rails",
+    :lib      => false,
+    :version  => ">= 1.2.0"
+  config.gem "cucumber",
+    :version  => ">= 0.3.98"
+  config.gem "webrat",
+    :version  => ">= 0.5.3"
+  
+  config.gem "haml",
+    :source   => github,
+    :version  => ">= 2.2.0"
+  config.gem "chriseppstein-compass",
+    :source   => github,
+    :version  => ">= 0.6.13",
+    :lib      => "compass"
+    
+  config.gem "mbleigh-seed-fu",
+    :source   => github,
+    :version  => ">= 1.0.0",
+    :lib      => "seed-fu"
+    
+  config.gem "RedCloth",
+    :version  => ">= 4.2.2"
+  
+  config.plugins = [ :haml, :all ]
+  
   # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
   # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = 'UTC'
